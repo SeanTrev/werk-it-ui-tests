26b74f63596f61ba2695ce10d486c1febf2b9976
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-02-11 12:30:01 -0700

    Removed all the other tests, and just focus on one.

--- src/test/java/com/xyzcorp/PlayWithCGI.java
+++ src/test/java/com/xyzcorp/PlayWithCGI.java
@@ -29,43 +29,7 @@ public class PlayWithCGI {
 
     @Test
     void testRelativeLocators() throws IOException {
-        InputStream resourceAsStream = this.getClass().getResourceAsStream("/locators.properties");
-        Properties properties = new Properties();
-        properties.load(resourceAsStream);
-
-
-        driver.get(
-                "https://www.cgi.com/en/search/site?keyword=business%20consulting");
-
-        WebElement articleContainer = driver.findElement(By.cssSelector(properties.getProperty("article-head-css")));
-        //sections inside the anchor
-        List<WebElement> sections = articleContainer.findElements(By.tagName("section"));
-
-        iterateAllSections(sections);
-    }
-
-    private void iterateAllSections(List<WebElement> sections) {
-        //for every section
-        for (WebElement element : sections) {
-            //get the line items
-            verifyTags(element);
-            verifyTitle(element);
-            verifyContent(element);
-        }
-    }
-
-    private void verifyContent(WebElement element) {
-
-    }
-
-    private void verifyTitle(WebElement element) {
-
-    }
-
-    private void verifyTags(WebElement element) {
-        List<WebElement> lineItems = element.findElements(By.cssSelector("div.tags > ul > li"));
-        for (WebElement lineItem : lineItems) {
-            System.out.println("******" + lineItem.getText());
-        }
+        driver.get("https://staging.tiered-planet.net/werk-it");
+        List<WebElement> sections = driver.findElements(By.tagName("section"));
     }
 }

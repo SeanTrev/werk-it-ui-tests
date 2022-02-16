70fbdd107e0ae7e8a8a9ca1786afb509818b943f
Sean Trevor <sean.trevor@cgi.com>
2022-02-16 00:20:12 -0500

    Updated APi and Selenium tests, Jenkins still not working

--- src/test/java/com/xyzcorp/werkitAPITests.java
+++ src/test/java/com/xyzcorp/werkitAPITests.java
@@ -13,26 +13,30 @@ import org.json.JSONObject;
 
 public class werkitAPITests {
     
+    //This test adds a user to the database
+    @Test
+    public void setUpUser(){
+        JSONObject userTest = new JSONObject()
+        .put("username", "testUser100")
+        .put("email", "testUser100@g.com")
+        .put("firstName", "Test")
+        .put("lastName", "User")
+        .put("password", "password");
 
-    // @Test
-    // public void setUpUser(){
-    //     JSONObject userTest = new JSONObject()
-    //     .put("username", "testUser111")
-    //     .put("email", "testUser111@g.com")
-    //     .put("firstName", "Test")
-    //     .put("lastName", "User")
-    //     .put("password", "password");
-
-    // given()
-    //         .relaxedHTTPSValidation()
-    //         .accept(ContentType.JSON)
-    //         .body(userTest.toString())
-    //         .when()
-    //         .post("https://staging.tiered-planet.net/werk-it-back-end/user/")
-    //         .then()
-    //         .assertThat();
-    //         //.statusCode(200);
-    // }
+    given()
+            .header("Content-Type", "application/json")
+            .header("Accept-Type", "application/json")
+            .log()
+            .all()
+            .relaxedHTTPSValidation()
+            .accept(ContentType.JSON)
+            .body(userTest.toString())
+            .when()
+            .post("https://staging.tiered-planet.net/werk-it-back-end/register")
+            .then()
+            .assertThat()
+            .statusCode(200);
+    }
 
     @Test
     public void testUser() {
@@ -40,19 +44,45 @@ public class werkitAPITests {
                 .relaxedHTTPSValidation()
                 .accept(ContentType.JSON)
                 .when()
-                .get("https://staging.tiered-planet.net/werk-it-back-end/weights/user/26")
+                .get("https://staging.tiered-planet.net/werk-it-back-end/weights/user/663")
                 .then()
                 .assertThat()
                 .body("[0].id", equalTo(612));
     }
 
     @Test
-    public void addExercise() {
+    public void deleteUser() {
+            given()
+                .log()
+                .all()
+                .relaxedHTTPSValidation()
+                .baseUri("https://staging.tiered-planet.net/werk-it-back-end")
+                .basePath("/weights/user")
+                .queryParam("id", "0")
+                .when()
+                //DELETE https://<endpoint-url>/storage/<collection>?ids=<ids>
+                .delete("/26")
+                .then()
+                .assertThat()
+                .statusCode(405); 
+    }
+
+    @Test
+	public void testDelete(){
+		given()
+		.when()
+		.delete("/1")
+		.then()
+		.header("Expires", equalTo("-1"));		
+	}
+
+    @Test
+    public void addExerciseWeight() {
         JSONObject userExerciseTest = new JSONObject()
-        .put("pounds", "40")
-        .put("reps", "20")
+        .put("pounds", "140")
+        .put("reps", "40")
         .put("sets", "2")
-        .put("name", "bench press");
+        .put("name", "Squat");
         System.out.println(userExerciseTest);
         given()
             .header("Content-Type", "application/json")
@@ -61,11 +91,77 @@ public class werkitAPITests {
             .accept(ContentType.JSON)
             .body(userExerciseTest.toString())
             .when()
-            .post("https://staging.tiered-planet.net/werk-it-back-end/weights/user/26")
+            .post("https://staging.tiered-planet.net/werk-it-back-end/weights/user/663")
             .then()
             .assertThat()
             .statusCode(200);
     }
 
+    @Test
+    public void addExerciseAerobics() {
+        JSONObject userExerciseTest = new JSONObject()
+        .put("name", "Yoga")
+        .put("seconds", "500");
+        System.out.println(userExerciseTest);
+        given()
+            .header("Content-Type", "application/json")
+            .header("Accept-Type", "application/json")
+            .relaxedHTTPSValidation()
+            .accept(ContentType.JSON)
+            .body(userExerciseTest.toString())
+            .when()
+            .post("https://staging.tiered-planet.net/werk-it-back-end/aerobics/user/663")
+            .then()
+            .assertThat()
+            .statusCode(200);
+    }
+
+    @Test
+    void testGetVerifyUserId(){
+        given()
+            .header("Content-Type", "application/json")
+            .header("Accept-Type", "application/json")
+            .relaxedHTTPSValidation()
+            .accept(ContentType.JSON)
+            .when()
+            .get("https://staging.tiered-planet.net/werk-it-back-end/aerobics/user/{id}", 663)
+            .then()
+            .statusCode(200)
+            .assertThat()
+            .body("[0].userId", equalTo(663));
+
+    }
+    @Test
+    void testGetExercise(){
+        given()
+            .header("Content-Type", "application/json")
+            .header("Accept-Type", "application/json")
+            .relaxedHTTPSValidation()
+            .accept(ContentType.JSON)
+            .when()
+            .get("https://staging.tiered-planet.net/werk-it-back-end/aerobics/user/{id}", 663)
+            .then()
+            .statusCode(200)
+            .assertThat()
+            .body("[0].name", equalTo("Yoga"));
 
 }
+@Test
+    void testGetExerciseWeight(){
+        given()
+            .header("Content-Type", "application/json")
+            .header("Accept-Type", "application/json")
+            .relaxedHTTPSValidation()
+            .accept(ContentType.JSON)
+            .when()
+            .get("https://staging.tiered-planet.net/werk-it-back-end/weights/user/{id}", 663)
+            .then()
+            .statusCode(200)
+            .assertThat()
+            .body("[0].name", equalTo("Squat"));
+
+}
+}
+
+
+
--- src/test/java/com/xyzcorp/werkitSeleniumTests.java
+++ src/test/java/com/xyzcorp/werkitSeleniumTests.java
@@ -1,4 +1,5 @@
 package com.xyzcorp;
+
 import java.time.Duration;
 
 import io.cucumber.java.BeforeAll;
@@ -8,11 +9,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver.Options;
@@ -27,50 +31,32 @@ import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
-
-
-
 public class werkitSeleniumTests {
-
     WebDriver driver;
 
-    @BeforeAll 
-    void setupAll(){
+    @BeforeAll
+    void setupAll() {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
-        driver = new ChromeDriver(options);
-        driver.manage().window().setSize(new org.openqa.selenium.Dimension(1920, 1080));
     }
 
+    // Before each test create driver and set window to standard size
     @BeforeEach
     void setup() {
         driver = WebDriverManager.chromedriver().create();
         driver.get("https://staging.tiered-planet.net/werk-it");
-        Dimension windowDim = driver.manage().window().getSize();
-        if (windowDim.getWidth() <= 992){
-            WebElement hamburger = driver.findElement(By.xpath("//*[@id='root']/div/div/div[1]/nav/div/button"));
-            hamburger.click();
-        }
-
+        Dimension defaultWindowSize = new Dimension(1920, 1080);
+        driver.manage().window().setSize(defaultWindowSize);
     }
 
+    // After each test wait 2 seconds and close driver
     @AfterEach
     void teardown() throws InterruptedException {
         Thread.sleep(Duration.ofSeconds(2).toMillis());
-
         driver.quit();
     }
 
-
-
-
-
-
-    @Test
-    void testRelativeLocators() throws IOException {
-        List<WebElement> sections = driver.findElements(By.tagName("section"));
-        
-    }
+    // Test all of the main buttons on app
     @Test
     void testHomeButton() {
         Actions actions = new Actions(driver);
@@ -81,8 +67,9 @@ public class werkitSeleniumTests {
         String siteURL = driver.getCurrentUrl();
         assertThat(siteURL).isEqualTo("https://staging.tiered-planet.net/werk-it");
     }
+
     @Test
-    void testLoginButton (){
+    void testLoginButton() {
         Actions actions = new Actions(driver);
         WebElement loginBtn = driver.findElement(By.xpath("//*[@id='navbarScroll']/div[1]/a[2]"));
         actions.moveToElement(loginBtn).build().perform();
@@ -91,6 +78,7 @@ public class werkitSeleniumTests {
         String siteURL = driver.getCurrentUrl();
         assertThat(siteURL).isEqualTo("https://staging.tiered-planet.net/werk-it/login");
     }
+
     @Test
     void testExercisesButton() {
         Actions actions = new Actions(driver);
@@ -101,6 +89,7 @@ public class werkitSeleniumTests {
         String siteURL = driver.getCurrentUrl();
         assertThat(siteURL).isEqualTo("https://staging.tiered-planet.net/werk-it/exercises");
     }
+
     @Test
     void testNutritionButton() {
         Actions actions = new Actions(driver);
@@ -111,6 +100,7 @@ public class werkitSeleniumTests {
         String siteURL = driver.getCurrentUrl();
         assertThat(siteURL).isEqualTo("https://staging.tiered-planet.net/werk-it/nutrition");
     }
+
     @Test
     void testCreateProfileButton() {
         Actions actions = new Actions(driver);
@@ -122,23 +112,12 @@ public class werkitSeleniumTests {
         assertThat(siteURL).isEqualTo("https://staging.tiered-planet.net/werk-it/register");
     }
 
+    // Go to create profile tab and create a new profile
     @Test
-    void testEmialIsValid (){
-        testCreateProfileButton();
-        WebElement emailTextField = driver.findElement(By.xpath("//*[@id='register']/div[3]/label/input"));
-        String emailValue = "TestUser111@g.com";
-        emailTextField.sendKeys(emailValue);
-
-
-        assertThat(emailTextField.getAttribute("value")).contains("@");
-    }
-   
-
-    @Test
-    void testCreateProfileAndLogin (){
+    void testCreateProfileAndLogin() {
         Actions actions = new Actions(driver);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        
+
         WebElement registerBtn = driver.findElement(By.xpath("//*[@id='navbarScroll']/div[2]/a[1]"));
         registerBtn.click();
 
@@ -174,8 +153,11 @@ public class werkitSeleniumTests {
         alert.accept();
     }
 
+    // Test Login of existing account
+    // Might be good idea to add an element that tells user what account is logged
+    // in
     @Test
-    void testLogin () {
+    void testLogin() {
         testHomeButton();
         WebElement usernameField = driver.findElement(By.xpath("//*[@id='username']"));
         String usernameValue = "TestUser111";
@@ -189,20 +171,94 @@ public class werkitSeleniumTests {
 
         WebElement submitCredentialsBtn = driver.findElement(By.xpath("//*[@id='login']/input"));
         submitCredentialsBtn.click();
+        String siteURL = driver.getCurrentUrl();
+
+        assertThat(siteURL).isEqualTo("https://staging.tiered-planet.net/werk-it");
 
     }
 
+    // Tests login page with an invalid password
+    @Test
+    void testLoginBadPassword() {
+        testHomeButton();
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
 
-    //Things to mention
-    //Must close hamburgers menu after selecting from it
-    //Must have min width and min height of 150x700 with hamburger closed too be able to click all buttons without scrolling
+        WebElement usernameField = driver.findElement(By.xpath("//*[@id='username']"));
+        String usernameValue = "TestUser111";
+        usernameField.sendKeys(usernameValue);
+        assertThat(usernameField.getAttribute("value")).isEqualTo(usernameValue);
+
+        WebElement passwordField = driver.findElement(By.xpath("//*[@id='password']"));
+        String passwordValue = "badpassword";
+        passwordField.sendKeys(passwordValue);
+        assertThat(passwordField.getAttribute("value")).isEqualTo(passwordValue);
+
+        WebElement submitCredentialsBtn = driver.findElement(By.xpath("//*[@id='login']/input"));
+        submitCredentialsBtn.click();
+        WebElement errorLogin = wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.xpath("//*[@id='root']/div/div/div[2]/p")));
+
+        assertThat(errorLogin.isDisplayed()).isTrue();
+    }
+
+    // Tests login with bad username
+    @Test
+    void testLoginBadUsername() {
+        testHomeButton();
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+
+        WebElement usernameField = driver.findElement(By.xpath("//*[@id='username']"));
+        String usernameValue = "TestUser111NotFound";
+        usernameField.sendKeys(usernameValue);
+        assertThat(usernameField.getAttribute("value")).isEqualTo(usernameValue);
+
+        WebElement passwordField = driver.findElement(By.xpath("//*[@id='password']"));
+        String passwordValue = "password";
+        passwordField.sendKeys(passwordValue);
+        assertThat(passwordField.getAttribute("value")).isEqualTo(passwordValue);
+
+        WebElement submitCredentialsBtn = driver.findElement(By.xpath("//*[@id='login']/input"));
+        submitCredentialsBtn.click();
+        WebElement errorLogin = wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.xpath("//*[@id='root']/div/div/div[2]/p")));
+
+        assertThat(errorLogin.isDisplayed()).isTrue();
+
+    }
+
+    // Tests login with no inputs in login fields
+    @Test
+    void testLoginNoInputs() {
+        testHomeButton();
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+        WebElement submitCredentialsBtn = driver.findElement(By.xpath("//*[@id='login']/input"));
+        submitCredentialsBtn.click();
+        WebElement errorLogin = wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.xpath("//*[@id='root']/div/div/div[2]/p")));
+        assertThat(errorLogin.isDisplayed()).isTrue();
+
+    }
+
+    // Things to mention
+    // Must close hamburgers menu after selecting from it
+    // Must have min width and min height of 150x700 with hamburger closed too be
+    // able to click all buttons without scrolling
     @Test
     void executeTestsWithSmallWindow() {
         driver.manage().window().setSize(new org.openqa.selenium.Dimension(400, 800));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         Dimension windowDim = driver.manage().window().getSize();
         WebElement hamburger = driver.findElement(By.xpath("//*[@id='root']/div/div/div[1]/nav/div/button"));
-        
+
         hamburger.click();
         testHomeButton();
         hamburger.click();
@@ -218,30 +274,93 @@ public class werkitSeleniumTests {
         hamburger.click();
         testCreateProfileAndLogin();
         hamburger.click();
+
+        hamburger.click();
+        testLoginNoInputs();
+        hamburger.click();
+
     }
-    
+
+    //Make sure there is no cookies on first visit to site
     @Test
-    void testNoCookiesWithoutAccount () {
+    void testNoCookiesWithoutAccount() {
         Options options = driver.manage();
         Set<Cookie> cookies = options.getCookies();
         assertThat(cookies).hasSize(0);
     }
+
+
+    //Check for the addition of the id cookie after login
     @Test
-    void testNoCookiesWithAccount () {
+    void testNoCookiesWithAccount() {
         testLogin();
         Options options = driver.manage();
         Set<Cookie> cookies = options.getCookies();
-        Cookie newCookie = new Cookie("id", "26");
-        options.addCookie(newCookie);
-        cookies = options.getCookies();
+        // Cookie newCookie = new Cookie("id", "26");
+        // options.addCookie(newCookie);
+        // cookies = options.getCookies();
         Cookie id = options.getCookieNamed("id");
         System.out.println("----------------------------------------------------------------");
         System.out.println(id);
-        
         assertThat(cookies).hasSize(1);
 
     }
-    
 
+
+    //This test should display warning when creating account if email is invalid
+    @Test
+    void testEmialIsValid() {
+        testCreateProfileButton();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement emailTextField = driver.findElement(By.xpath("//*[@id='register']/div[3]/label/input"));
+        String emailValue = "TestUser111@g.com";
+        emailTextField.sendKeys(emailValue);
+        if(emailTextField.getAttribute("value").contains("@")){
+            System.out.println("Valid Email");
+        }else{
+            System.out.println("Not Valid Email");
+            //WebElement emailNotValidWarning = driver.findElement(By.id("emailNotValidWarning"));
+
+        }
+
+        }
+
+
+
+
+    //This test would test for an alret when trying to sign in with an invalid email address
+    //     // Should have alert that pops up if submit is clicked and email is not valid
+    //     WebElement nameTextField = driver.findElement(By.xpath("//*[@id='register']/div[1]/label/input"));
+    //     String nameValue = "bademail";
+    //     nameTextField.sendKeys(nameValue);
+    //     assertThat(nameTextField.getAttribute("value")).isEqualTo(nameValue);
+
+    //     WebElement lastNameTextField = driver.findElement(By.xpath("//*[@id='register']/div[2]/label/input"));
+    //     String lastNameValue = "bademail";
+    //     lastNameTextField.sendKeys(lastNameValue);
+    //     assertThat(lastNameTextField.getAttribute("value")).isEqualTo(lastNameValue);
+
+    //     WebElement bademailTextField = driver.findElement(By.xpath("//*[@id='register']/div[3]/label/input"));
+    //     String bademailValue = "bademail.com";
+    //     bademailTextField.sendKeys(bademailValue);
+    //     assertThat(bademailTextField.getAttribute("value")).isEqualTo(bademailValue);
+
+    //     WebElement usernameField = driver.findElement(By.xpath("//*[@id='register']/div[4]/label/input"));
+    //     String usernameValue = "bademail";
+    //     usernameField.sendKeys(usernameValue);
+    //     assertThat(usernameField.getAttribute("value")).isEqualTo(usernameValue);
+
+    //     WebElement passwordField = driver.findElement(By.xpath("//*[@id='register']/div[5]/label/input"));
+    //     String passwordValue = "bademail";
+    //     passwordField.sendKeys(passwordValue);
+    //     assertThat(passwordField.getAttribute("value")).isEqualTo(passwordValue);
+
+    //     WebElement submitCredentialsBtn = driver.findElement(By.xpath("//*[@id='register']/input"));
+    //     submitCredentialsBtn.click();
+    //     Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+    //     assertThat(alert.getText()).contains("Invalid Email Address");
+    //     alert.accept();
+
+    // }
 
 }
